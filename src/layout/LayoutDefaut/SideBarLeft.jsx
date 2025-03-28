8c19f0424a5971386accee57bc8fa6208bf1caf8
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-03-29 00:37:26 +0700

    TIT

--- src/layout/LayoutDefaut/SideBarLeft.jsx
+++ src/layout/LayoutDefaut/SideBarLeft.jsx
@@ -1,16 +1,36 @@
+import React from "react";
 import { forwardRef } from "react";
 import { cn } from "../../util/cn";
-
-const SideBarLeft = forwardRef(({}, ref) => {
+import logoDark from "../../assets/Img/dashboard_Dark.svg";
+import logoLight from "../../assets/Img/dashboard_Light.svg";
+import PropTypes from "prop-types";
+const SideBarLeft = forwardRef(({ collapsed }, ref) => {
     return (
         <aside
             ref={ref}
             className={cn(
                 "fixed z-[100] flex h-full w-[240px] flex-col overflow-x-hidden border-r border-slate-300 bg-white transition-[width_300ms_cubic-bezier(0.4,0,0.2,1)_left_300ms_cubic-bezier(0.4,0,0.2,1)_background-color_150ms_cubic-bezier(0.4,0,0.2,1)_border_150ms_cubic-bezier(0.4,0,0.2,1)] dark:border-slate-700 dark:bg-slate-900",
             )}
-        ></aside>
+        >
+            <div className="flex h-[50px] w-[50px] gap-x-3 p-3">
+                <img
+                    src={logoLight}
+                    alt="LogoLight"
+                    className="dark:hidden"
+                />
+                <img
+                    src={logoDark}
+                    alt="LogoLight"
+                    className="hidden dark:block"
+                />
+                {!collapsed && <p className="text-lg font-medium text-slate-900 transition-colors dark:text-slate-50">DASHBOARD</p>}
+            </div>
+        </aside>
     );
 });
 
 SideBarLeft.displayName = "Sidebar";
+SideBarLeft.prototype = {
+    collapsed: PropTypes.bool,
+};
 export default SideBarLeft;
--- src/layout/LayoutDefaut/index.jsx
+++ src/layout/LayoutDefaut/index.jsx
@@ -1,14 +1,22 @@
-import React from "react"; // Ensure React is imported
+import React, { useRef, useState } from "react"; // Ensure React is imported
 import { Outlet } from "react-router-dom";
-
+import { useMediaQuery } from "@uidotdev/usehooks";
 import { cn } from "../../util/cn";
 import Header from "./Header";
 import SideBarLeft from "./SideBarLeft";
 
 function LayoutDefault() {
+    const isDesktopDevice = useMediaQuery("(min-width:768px)");
+    const [collapsed, setCollapsed] = useState(false);
+
+    const sidebarRef = useRef(null);
+
     return (
         <div className="min-h-screen bg-slate-100 transition-colors dark:bg-slate-900">
-            <SideBarLeft />
+            <SideBarLeft
+                ref={sidebarRef}
+                collapsed={collapsed}
+            />
             <div className={cn("transition-[margin] duration-300")}>
                 <Header />
                 <div className="h-[calc(100vh-60px) overflow-x-hidden overflow-y-auto p-6">
